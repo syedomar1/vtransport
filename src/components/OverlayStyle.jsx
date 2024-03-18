118e21a196aa12ab0be7e8aba23b74d0b94cc54d
Syed Omar Albeez <syedomar.albeez2022@vitstudent.ac.in>
2024-03-18 23:20:34 +0530

    Updated styling

--- src/components/OverlayStyle.jsx
+++ src/components/OverlayStyle.jsx
@@ -1,46 +1,55 @@
-import Logo from "./Logo";
+import React from "react";
 import TitleCard from "./TitleCard";
-export default function OverlayStyle(){
-    const overlayStyle = {
+
+export default function OverlayStyle() {
+    // const overlayStyle = {
+    //     position: 'relative',
+    //     width: '100vw',
+    //     height: '100vh',
+    // };
+
+    const imageStyle = {
         position: 'absolute',
         top: 0,
         left: 0,
-        width: '100vw',
-        height: '100vh',
+        width: '100%',
+        height: '100%',
+        backgroundImage: 'url("./public/vit_bg.png")',
+        backgroundRepeat: 'no-repeat',
         backgroundPosition: 'center',
-        backgroundColor: 'rgba(19, 19, 19, 0.2)', 
-        zIndex: -1, 
-        backgroundSize:'cover',
-      };
+        backgroundSize: 'cover',
+        zIndex: 0,
+    };
+
     const dashBoard = {
         position: 'absolute',
-        top: 200,
-        left: 700,
+        top: '30vh',
+        left: 'calc(50% - 20vw)',
         width: '40vw',
         height: '28vw',
         backgroundColor: 'rgba(0,0,0,0.5)',
         color: 'white',
-        padding: 10,
-        borderRadius:"15px"
+        padding: '2.5vw',
+        borderRadius: '3.75vw',
+        zIndex: 1,
     };
 
     return (
-        <div className="OverlayStyle" style={overlayStyle}>
-            {/* <Logo></Logo> */}
-            <TitleCard></TitleCard>
-            <div style = {dashBoard}>
+        <div className="OverlayStyle" >
+            <TitleCard />
+            <div style={imageStyle}></div>
+            <div style={dashBoard}>
                 <h1 className="text-3xl font-semibold">DASHBOARD</h1>
-                <hr className="mt-2 border-2 w-76 color-white"/>
+                <hr className="mt-2 border-2 w-76 color-white" />
                 <ul className="py-5 list-disc px-5 text-xl">
-                    <li>Due to the red alert being declared in chennai all the buses will leave at 4:00pm.
-                        <hr className="mt-2 border-1 w-76 color-white"/>
+                    <li>Due to the red alert being declared in Chennai, all the buses will leave at 4:00pm.
+                        <hr className="mt-2 border-1 w-76 color-white" />
                     </li>
-                    <li>Please make sure all the students who used the college bus get their bus pass from the transport
-                        office before 2:00pm without fail.
-                        <hr className="mt-2 border-1 w-76 color-white"/>
+                    <li>Please make sure all the students who used the college bus get their bus pass from the transport office before 2:00pm without fail.
+                        <hr className="mt-2 border-1 w-76 color-white" />
                     </li>
                 </ul>
             </div>
-        </div>
-    )
+        </div>
+    );
 }
